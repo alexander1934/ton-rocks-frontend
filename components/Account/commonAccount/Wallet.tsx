4c6c9fa2b6859af7eed6b030b9f2ce15db03b793
Alexander Kovalenko <alexankovalenko23@gmail.com>
2023-06-02 16:36:19 +0300

    added reducer functions

--- components/Account/commonAccount/Wallet.tsx
+++ components/Account/commonAccount/Wallet.tsx
@@ -5,28 +5,35 @@ import getIcon from "@/public/walletButtons/getButton.svg";
 import sendIcon from "@/public/walletButtons/sendButton.svg";
 import depositIcon from "@/public/walletButtons/depositButton.svg";
 import React from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {getMoney, sendMoney} from "@/store/walletSlice";
 
 
 const Wallet: React.FC = () => {
 
     // @ts-ignore
-    const state = useSelector(state => state.wallet.balance);
+    const state = useSelector(state => state.wallet);
+    const dispatch = useDispatch();
+
+    const addMoney = () => dispatch(getMoney(state.balance));
+    const lostMoney = () => dispatch(sendMoney(state.balance));
+
+    const balanceTON = state.balance / 142.98
 
     return (
         <div className={`${s.gridElement} ${s.wallet}`}>
             <div className={s.balanceLine}>Баланс:</div>
             <div className={s.balanceBlock}>
-                <div className={s.cryptoBalance}>0 Tokens</div>
-                <div className={s.currencyBalance}>В рублях: {state}₽</div>
+                <div className={s.cryptoBalance}>{balanceTON} Tokens</div>
+                <div className={s.currencyBalance}>В рублях: {state.balance}₽</div>
             </div>
             <div className={s.buttonsBlock}>
                 <div className={s.buttonGroup}>
-                    <Button className={s.buttonElement}><Image src={getIcon} alt={""}/></Button>
+                    <Button className={s.buttonElement} onClick={addMoney}><Image src={getIcon} alt={""}/></Button>
                     <p>Получить</p>
                 </div>
                 <div className={s.buttonGroup}>
-                    <Button className={s.buttonElement}><Image src={sendIcon} alt={""}/></Button>
+                    <Button className={s.buttonElement} onClick={lostMoney}><Image src={sendIcon} alt={""}/></Button>
                     <p>Отправить</p>
                 </div>
                 <div className={s.buttonGroup}>
--- store/walletSlice.tsx
+++ store/walletSlice.tsx
@@ -3,9 +3,17 @@ import {createSlice} from "@reduxjs/toolkit";
 const walletSlice = createSlice({
     name: "wallet",
     initialState: {
-        balance: 1000
+        balance: 10
     },
-    reducers: {}
+    reducers: {
+        getMoney(state, action): void{
+            state.balance += 1000
+        },
+        sendMoney(state, action): void{
+            state.balance -= 1000
+        },
+    }
 })
 
+export const {getMoney, sendMoney} = walletSlice.actions;
 export default walletSlice.reducer;
